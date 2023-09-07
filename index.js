5419bd517a305ba878386c9e68b92d4ed4f5cd47
Maria <mvalenciacuberos@gmail.com>
2023-09-07 17:28:59 +0200

    Make array of first 10 links

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ mkdir('./Memes', { recursive: true }, (err) => {
   if (err) throw err;
 });
 
-const myUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
+// const myUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
 
 // this gets me the entire html from the website, it's a bit too much
 const response = await fetch(
@@ -20,4 +20,30 @@ const root = parse(body);
 
 const getImages = root.querySelectorAll('img').toString();
 
-console.log(getImages);
+// console.log(getImages);
+
+// const tagOpen = /<img *src=/g;
+// const tagClose = / *>,/g;
+
+// function cleanHtml() {
+//   const minusFrontTag = getImages.replaceAll(tagOpen, '');
+//   const minusEndTag = getImages.replaceAll(tagClose, '|');
+//   console.log(toString(minusFrontTag + minusEndTag));
+// }
+
+// console.log(cleanHtml());
+
+// const imageLinks = getImages.replaceAll(/ *>,<img *src=/g, '|');
+
+// console.log(imageLinks.split('|', 10));
+
+// console.log(getImages.replaceAll(/ *>,<img *src=/g, '|'));
+
+// console.log(getImages.replaceAll(tagOpen, ''));
+// getImages.replaceAll(tagClose, '|'));
+
+// console.log(getImages.split(tagClose, 10));
+
+const getLinks = getImages.match(/https.*?00/g);
+const getTenLinks = getLinks.slice(0, 11);
+console.log(getTenLinks);
