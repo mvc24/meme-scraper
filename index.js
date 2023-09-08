6c9bf9580b5241457afdc3d43aa2af4af8566848
Maria <mvalenciacuberos@gmail.com>
2023-09-08 16:22:17 +0200

    Fix code to download all images

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 import { Buffer } from 'node:buffer';
 import { mkdir, writeFile } from 'node:fs';
+import https from 'node:https';
 import path from 'node:path';
 import url from 'node:url';
+import cheerio from 'cheerio';
 import * as fs from 'fs';
 import fetch from 'node-fetch';
 import { parse } from 'node-html-parser';
 
-/* const data = new Uint8Array(Buffer.from('Hello Node.js'));
-writeFile('message.txt', data, (err) => {
-  if (err) throw err;
-  console.log('The file has been saved!');
-}); */
-
-mkdir('./Memes', { recursive: true }, (err) => {
+mkdir('./memes', { recursive: true }, (err) => {
   if (err) throw err;
 });
 
-// const myUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
-
-// this gets me the entire html from the website, it's a bit too much
 const response = await fetch(
   'https://memegen-link-examples-upleveled.netlify.app/',
 );
@@ -27,6 +20,78 @@ const body = await response.text();
 const root = parse(body);
 
 const getImages = root.querySelectorAll('img').toString();
+
+const getLinks = getImages.match(/https.*?00/g);
+const getTenLinks = getLinks.slice(0, 11);
+
+const linkArray = [];
+
+for (let link of getTenLinks) {
+  linkArray.push(link);
+}
+
+let fileName = '';
+
+const fileNameArray = [];
+
+for (let i = 1; i <= 10; i++) {
+  if (i < 10) {
+    fileName = `0${i}.jpg`;
+    // console.log(fileName);
+    fileNameArray.push(fileName);
+  } else {
+    fileName = `${i}.jpg`;
+    // console.log(fileName);
+    fileNameArray.push(fileName);
+  }
+}
+for (let i = 0; i <= 10; i++) {
+  fetch(linkArray[i])
+    .then((res) => res.buffer())
+    // console.log('value of response', res.url);
+    .then((buffer) => {
+      // Write the buffer to a file;
+      fs.writeFile(path.join('./memes', fileNameArray[i]), buffer, (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log('Image downloaded successfully');
+        }
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+// let a = fileNameArray;
+// let b = linkArray;
+
+/* const nameLinkObject = (a, b) => {
+  let obj = {};
+  a.forEach((k, i) => {
+    obj[k] = b[i];
+  });
+  return obj;
+};
+console.log(nameLinkObject(a, b)); */
+
+// let link = linkArray[0];
+
+// for (let links = )
+
+/* for (let i = 0; i <= 10; i++) {
+  let
+
+  fs.writeFile(fileNameArray[i], linkArray[i], (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('file saved');
+  });
+} */
+
+// fs.writeFile(dirPath);
 
 // console.log(getImages);
 
@@ -51,69 +116,3 @@ const getImages = root.querySelectorAll('img').toString();
 // getImages.replaceAll(tagClose, '|'));
 
 // console.log(getImages.split(tagClose, 10));
-
-const getLinks = getImages.match(/https.*?00/g);
-const getTenLinks = getLinks.slice(0, 11);
-// console.log(getTenLinks);
-
-// getTenLinks.forEach((link) => console.log(link));
-// it doesn't like the link
-
-const linkArray = [];
-
-for (let link of getTenLinks) {
-  linkArray.push(link);
-}
-
-// console.log(linkArray);
-
-let fileName = '';
-
-const fileNameArray = [];
-
-for (let i = 1; i <= 10; i++) {
-  if (i < 10) {
-    fileName = `0${i}.jpg`;
-    // console.log(fileName);
-    fileNameArray.push(fileName);
-  } else {
-    fileName = `${i}.jpg`;
-    // console.log(fileName);
-    fileNameArray.push(fileName);
-  }
-}
-
-let a = fileNameArray;
-let b = linkArray;
-
-const nameLinkObject = (a, b) => {
-  let obj = {};
-  a.forEach((k, i) => {
-    obj[k] = b[i];
-  });
-  return obj;
-};
-console.log(nameLinkObject(a, b));
-
-const dirPath = './Memes';
-
-// fs.writeFile(dirPath);
-
-/* let imageUrl = fetch(linkArray)
-  .then((res) => {
-    // res.arrayBuffer();
-    console.log('value of response', res.url);
-  })
-  .then((buffer) => {
-    // Write the buffer to a file
-    fs.writeFile(path.join(dirPath, fileNameArray[0]), buffer, (err) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log('Image downloaded successfully');
-      }
-    });
-  })
-  .catch((error) => {
-    console.error(error);
-  }); */
